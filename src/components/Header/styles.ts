35ae5c5e49936bc99f0b10f13704a0b932531325
Alexandre Garcia <alexandre.garciaa@hotmail.com>
2022-11-12 15:10:54 -0300

    Responsividade

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -8,9 +8,21 @@ export const Container = styled.div`
         margin: 0;
         padding: 0;
         font-size: 28px;
+        margin-left: 30px;
     }
     p {
         font-size: 14px;
         color: #b8b8d4;
+        margin-left: 30px;
     }
+
+    @media (max-width: 500px) {
+        h1 {
+            font-size: 20px;
+        }
+        P {
+            font-size: 11px;
+        }
+    }
+
 `;
